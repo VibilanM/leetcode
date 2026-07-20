11a8d07a5a747c7fb792cc47148f82591e1eabbe
Vibilan <vibilan2793@gmail.com>
2026-07-20 20:15:16 +0530

    jul 20

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,5 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
