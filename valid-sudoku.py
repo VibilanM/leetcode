81934189955d5dde6910cd74422c19b45f14df51
Vibilan <vibilan2793@gmail.com>
2026-07-25 09:34:51 +0530

    25 jul

--- valid-sudoku.py
+++ valid-sudoku.py
@@ -1,37 +1,37 @@
-from collections import Counter
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according 
+# to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 
 class Solution(object):
     def isValidSudoku(self, board):
-        for i in board:
-            c = Counter(i)
-            del c['.']
-            if c.values() != []:
-                if max(c.values()) != 1:
+        rows = [set() for i in range(9)]
+        cols = [set() for j in range(9)]
+        boxes = [set() for k in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+
+                if num == ".":
+                    continue
+
+                if num in rows[r]:
                     return False
 
-        cols = [[] for _ in range(9)]
-        for j in board:
-            for k in range(9):
-                cols[k].append(j[k])
-        
-        for x in cols:
-            d = Counter(x)
-            del d['.']
-            if d.values() != []:
-                if max(d.values()) != 1:
+                if num in cols[c]:
                     return False
-        
-        threes = [[] for _ in range(9)]
-        for y in range(9):
-            for z in range(9):
-                box_index = (y // 3) * 3 + (z // 3)
-                threes[box_index].append(board[y][z])
 
-        for a in threes:
-            e = Counter(a)
-            del e['.']
-            if e.values() != []:
-                if max(e.values()) != 1:
+                box = ((r //3) * 3) + (c // 3)
+
+                if num in boxes[box]:
                     return False
+
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[box].add(num)
 
         return True
+        
