d1e9d490cef9f8dc11b3057daf19de15cefbe836
Vibilan <vibilan2793@gmail.com>
2026-08-11 08:30:41 +0530

    11 aug

---
+++ others/maximum-achievable-number.py
@@ -0,0 +1,8 @@
+# Given two integers, num and t. A number x is achievable if it can become equal to num after applying the following operation at most t times:
+
+# Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+# Return the maximum possible value of x.
+
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        return num + (2 * t)
