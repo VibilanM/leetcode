b571e250bce50dd6b5141b651aea44951bfec1a7
Vibilan <vibilan2793@gmail.com>
2026-08-14 23:28:32 +0530

    14 aug 2

---
+++ others/mirror-distance-of-an-integer.py
@@ -0,0 +1,11 @@
+# You are given an integer n.
+
+# Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
+
+# Return an integer denoting the mirror distance of n​​​​​​​.
+
+# abs(x) denotes the absolute value of x.
+
+class Solution(object):
+    def mirrorDistance(self, n):
+        return abs(n - int(str(n)[::-1]))
