8fa80ea53618be52723e0b26fa9c1ff83eed06a4
Vibilan <vibilan2793@gmail.com>
2026-08-14 23:33:02 +0530

    14 aug 3

---
+++ others/concatenate-array-with-reverse.py
@@ -0,0 +1,13 @@
+# You are given an integer array nums of length n.
+
+# Construct a new array ans of length 2 * n such that the first n elements are the same as nums, and the next n elements are the elements of nums in reverse order.
+
+# Formally, for 0 <= i <= n - 1:
+
+# ans[i] = nums[i]
+# ans[i + n] = nums[n - i - 1]
+# Return an integer array ans.
+
+class Solution(object):
+    def concatWithReverse(self, nums):
+        return nums + nums[::-1]
