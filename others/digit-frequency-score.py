3936a05e6f253849891343f57c30a9a2070e25dc
Vibilan <vibilan2793@gmail.com>
2026-08-14 23:27:06 +0530

    14 aug

---
+++ others/digit-frequency-score.py
@@ -0,0 +1,9 @@
+# You are given an integer n.
+
+# The score of n is defined as the sum of d * freq(d) over all distinct digits d, where freq(d) denotes the number of times the digit d appears in n.
+
+# Return an integer denoting the score of n.
+
+class Solution(object):
+    def digitFrequencyScore(self, n):
+        return sum(map(int, str(n)))
